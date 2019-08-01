afd7301949ba764d81a1786490b1a018d197d45a
Kingsley Victor <javaprodigy56@gmail.com>
2019-08-01 13:11:16 -0700

    chore: add view for unsupported viewports

---
+++ UI/static/js/view-not-supported.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', (event) => {
+  if (window.screen.width <= 500) {
+    location.assign('/');
+  }
+});
+
+window.addEventListener('load', (event) => {
+  if (window.screen.width <= 500) {
+    location.assign('/');
+  }
+});
